05b2270b83facf3644561d9e43327ba0cd6970c5
Javier Gomez <javier@codigoweb.es>
2023-02-09 13:52:23 +0100

    Refactoring the consumption calculation

--- src/InfluxdbService.php
+++ src/InfluxdbService.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\influxdb;
 
+use DateInterval;
 use DateTime;
 use Drupal;
 use Drupal\Component\Serialization\Json;
@@ -31,6 +32,9 @@ class InfluxdbService {
   /**
    * POST Document.
    *
+   * @param $url
+   * @param $body
+   * @param $token
    * @return array
    *   The service response.
    */
@@ -62,22 +66,38 @@ class InfluxdbService {
     return $response;
   }
 
-  public function getRangeValues($influxdb_id, $dbname, $measurement, $name, $channel, $start_date, $end_date) {
+  /**
+   * @param $dbname
+   * @param $measurement
+   * @param $name
+   * @param $channel
+   * @param $start_date
+   * @param $end_date
+   * @return string
+   */
+  public function getCommonBody( $dbname, $measurement, $name, $channel, $start_date, $end_date) {
+
+    return 'from(bucket: "'. $dbname. '")
+  |> range(start: '. $start_date .', stop: '. $end_date .')
+  |> filter(fn: (r) => r._measurement == "' . $measurement . '")
+  |> filter(fn: (r) => r._field =="name" or r._field =="channel" or r._field =="value")
+  |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")
+  |> filter(fn: (r) => r.name == "' . $name . '")
+  |> filter(fn: (r) => r.channel == ' . $channel . ')';
+  }
+
+  /**
+   * @param $influxdb_id
+   * @param $body
+   * @return array
+   */
+  public function getResults($influxdb_id, $body) {
 
     $influxdbConnection = InfluxdbConnection::load($influxdb_id);
     if ($influxdbConnection instanceof InfluxdbConnectionInterface) {
       $url = $influxdbConnection->getUrlApiQueryV2();
       $token = $influxdbConnection->getToken();
     }
-
-    $body = 'from(bucket: "'. $dbname. '")
-  |> range(start: '. $start_date .', stop: '. $end_date .')
-  |> filter(fn: (r) => r._measurement == "' . $measurement . '")
-  |> filter(fn: (r) => r._field =="name" or r._field =="channel" or r._field =="value")
-  |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")
-  |> filter(fn: (r) => r.name == "' . $name . '")
-  |> filter(fn: (r) => r.channel == ' . $channel . ')
-';
 
     $result = [];
     $messages = $this->post($url, $body, $token);
@@ -98,9 +118,82 @@ class InfluxdbService {
       }
     }
     return $result;
-
   }
 
+  /**
+   * @param $influxdb_id
+   * @param $dbname
+   * @param $measurement
+   * @param $name
+   * @param $channel
+   * @param $start_date
+   * @param $end_date
+   * @return array
+   */
+  public function getFirstValue($influxdb_id, $dbname, $measurement, $name, $channel, $start_date, $end_date) {
+
+    $body = $this->getCommonBody($dbname, $measurement, $name, $channel, $start_date, $end_date);
+    $body .= '  |> limit(n: 1)';
+    return $this->getResults($influxdb_id, $body);
+  }
+
+  /**
+   * @param $influxdb_id
+   * @param $dbname
+   * @param $measurement
+   * @param $name
+   * @param $channel
+   * @param $start_date
+   * @param $end_date
+   * @return array
+   */
+  public function getLastValue($influxdb_id, $dbname, $measurement, $name, $channel, $start_date, $end_date) {
+
+    $body = $this->getCommonBody($dbname, $measurement, $name, $channel, $start_date, $end_date);
+    $body .= '  |> last(column: "_time")';
+    return $this->getResults($influxdb_id, $body);
+  }
+
+  /**
+   * Date range
+   *
+   * @param DateTime $first
+   * @param DateTime $last
+   * @param string $step
+   * @param string $format
+   * @return array
+   */
+  public function getDateRange( DateTime $first, DateTime $last, $step = '+1 day') {
+    $dates = [];
+    $current = $first;
+    $interval = new DateInterval('P1D');
+    $interval2 = new DateInterval('PT1H');
+
+    $i = 0;
+    while( $current < $last ) {
+      $dates[$i]['begin'] = $current->format(DateTime::RFC3339);
+      $current->add($interval2);
+      $dates[$i]['end'] = $current->format(DateTime::RFC3339);
+      if ($current === $first) {
+        $current->setTime(0,0,0);
+      }
+      $current->add($interval);
+      $i++;
+    }
+    $dates[$i]['end'] = $last->format(DateTime::RFC3339);
+    $last->sub($interval2);
+    $dates[$i]['begin'] = $last->format(DateTime::RFC3339);
+
+    return $dates;
+  }
+
+  /**
+   * @param NodeInterface $pitch
+   * @param DateTime $start_date
+   * @param DateTime $end_date
+   * @return array
+   * @throws \Exception
+   */
   public function getConsumption(NodeInterface $pitch, DateTime $start_date, DateTime $end_date) {
 
     // Recuperamos los datos del Pitch y del Grupo
@@ -117,67 +210,51 @@ class InfluxdbService {
     $pvp_kwh = (float) $group->get('field_pvp_kwh')->value;
     $influxdb_id = $group->get('field_influxdb_connection')->target_id;
 
+    // Adjust the dates to UTC time
+    $interval =new DateInterval('PT' . $start_date->getOffset() . 'S');
+    $start_date->add($interval);
+    $end_date->add($interval);
     $start_date->setTimezone(new \DateTimeZone('UTC'));
     $end_date->setTimezone(new \DateTimeZone('UTC'));
-    $start_date_rfc = $start_date->format(DateTime::RFC3339);
+
+    $dates = $this->getDateRange($start_date, $end_date);
     $end_date_rfc = $end_date->format(DateTime::RFC3339);
-
-    // Recogemos todos los datos entre las fechas señaladas
-    $data = $this->getRangeValues($influxdb_id, $dbname, $measurement, $name, $channel, $start_date_rfc, $end_date_rfc);
-    $start_value = reset($data);
-    $start_date = array_key_first($data);
-    $end_value = end($data);
-    $end_date = array_key_last($data);
-    // inicializamos las variables para realizar los cálculos
-    $previous_value = 0;
-    $compare_partial_value = $start_value;
+    $values = [];
+    foreach ($dates as $id => $date) {
+      if ($date['begin'] !== $end_date_rfc) {
+        $datepart = substr($date['begin'], 0, 10);
+        $values[$id]['date'] = $datepart;
+        $initial_value = $this->getFirstValue($influxdb_id, $dbname, $measurement, $name, $channel, $date['begin'], $date['end']);
+        $n = 1;
+        while( empty($initial_value)) {
+          $initial_value = $this->getFirstValue($influxdb_id, $dbname, $measurement, $name, $channel, $date['begin'], $dates[$id+$n]["begin"]);
+          $n++;
+        }
+        $values[$id]['initial_value'] = reset($initial_value);
+        if ($id !== 0) {
+          $values[$id-1]['end_value'] = $values[$id]['initial_value'];
+          $values[$id-1]['consumption'] = $values[$id-1]['end_value'] - $values[$id-1]['initial_value'];
+        }
+      }
+      else {
+        $end_value = $this->getLastValue($influxdb_id, $dbname, $measurement, $name, $channel, $date['begin'], $date['end']);
+        if (empty($end_value)) {
+          $end_value = $this->getLastValue($influxdb_id, $dbname, $measurement, $name, $channel, $dates[$id-1]["end"], $date['end']);
+        }
+        $values[$id-1]['end_value'] = reset($end_value);;
+        $values[$id-1]['consumption'] = $values[$id-1]['end_value'] - $values[$id-1]['initial_value'];
+      }
+    }
     $consumption = 0;
-    $consumption_partial = 0;
-    $compare_value = $start_value;
-    $compare_date = substr($start_date, 0, 10);
+    foreach ($values as $value) {
+      $consumption += $value["consumption"];
+      $comsuptions[$value["date"]] = round($value["consumption"], 4);
 
-    foreach ($data as $date => $value) {
-      // Si hay cortes de luz, el contador empieza de 0
-      if ($value < $previous_value) {
-        $consumption += $previous_value - $compare_value;
-        $compare_value = $value;
-        $compare_partial_value = $value;
-      }
-      $previous_value = $value;
-
-      // Calculamos los datos de cada día por comparación con el día anterior (menos el último)
-      $partial_date = substr($date, 0, 10);
-      if ($partial_date <> $compare_date) {
-        $comsuptions[$compare_date] = round($value - $compare_partial_value, 4);
-        $compare_partial_value = $value;
-        $consumption_partial += $comsuptions[$compare_date];
-        $compare_date = $partial_date;
-      }
     }
-
-    // acumulamos la diferencia cuando cambia el valor recibido
-    if ($compare_value !== $start_value) {
-      $consumption += $end_value - $compare_value;
-    }
-    // Si el valor es 0 es que ha habido corte de luz y
-    if ($consumption === 0) {
-      $consumption = $end_value - $start_value;
-    }
-
-    // Calculamos los datos del último día
-    $last_date = substr($end_date, 0, 10);
-    if ($last_date === $compare_date) {
-      $comsuptions[$last_date] = round($consumption - $consumption_partial, 4);
-    }
-    // Añadimos el consumo total al array devuelto
     $comsuptions['total'] = round($consumption, 4);
     $comsuptions['pvp_kwh'] = $pvp_kwh;
     $comsuptions['importe'] = round($consumption / 1000 * $pvp_kwh, 2);
 
     return Json::encode($comsuptions);
-
   }
-
-
-
 }
